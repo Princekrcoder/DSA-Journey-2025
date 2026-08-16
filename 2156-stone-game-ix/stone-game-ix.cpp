fc52c55f49d227759764fa4b798bed5b69b3b9cc
Prince Kumar <princekrcoder@gmail.com>
2026-08-16 23:59:33 +0530

    Time: 4 ms (62.44%) | Memory: 131.3 MB (33.50%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.cpp
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -7,15 +7,10 @@ public:
             cnt[x % 3]++;
         }
 
-        // If there are no remainder 1 and 2 stones,
-        // Alice cannot make the sum divisible by 3 strategically.
         if (cnt[1] == 0 && cnt[2] == 0) {
             return false;
         }
 
-        // If cnt[0] is even, the game is balanced.
-        // Alice can win if there is enough imbalance between
-        // remainder-1 and remainder-2 stones.
         if (cnt[0] % 2 == 0) {
             return cnt[1] > 0 && cnt[2] > 0;
         }
